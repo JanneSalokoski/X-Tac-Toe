2a6cbd1c8fae3ab1ec4106b7fe277ca59915b643
Janne Salokoski <janne@salokoski.fi>
2016-03-20 08:49:42 +0200

    Implement Squares state

--- res/js/engine.js
+++ res/js/engine.js
@@ -1,6 +1,7 @@
 Square = function(x_position, y_position)
 {   // Position is local because it shouldn't be tampered with manually
     var position = {};
+    var state = {};
 
     // You can only _get_ the position of a Square from the outside
     this.getPosition = function (pos)
@@ -29,12 +30,79 @@ Square = function(x_position, y_position)
         }
     }
 
+    // initialization should obviously be executed only locally
     var initializePosition = function ()
     {
         if (typeof x_position == "number" && typeof y_position == "number")
         {
             setPosition({"x": parseInt(x_position), "y": parseInt(y_position)});
         }
+    }
+
+    // No one global needs to see the whole state, right?
+    var getState = function (item)
+    {   // Check if we have got an item
+        if (typeof item == "undefined")
+        {
+            return state; // If not we return the whole state
+        }
+        else if (item.toLowerCase() == "user" || item.toLowerCase() == "value")
+        {   // Otherwise return either user or value, however the user wants
+            return state[item.toLowerCase()];
+        }
+    }
+
+    // Nor should they set the object this directly.
+    var setState = function (value, item)
+    {
+        if (typeof item == "undefined" && typeof value == "object")
+        {   // If value is an object, set state accordingly
+            state.user = value.user || value[0];
+            state.value = value.value || value[1];
+        }
+        else
+        {   // Otherwise set state[item] = value
+            state[item.toLowerCase()] = value;
+        }
+    }
+
+    // Get user and value are dummy functions for getState
+    this.getUser = function ()
+    {
+        var state = getState("user");
+
+        if (typeof state != "undefined")
+        {   // If we have a user, return it
+            return state;
+        }
+        else
+        {   // Otherwise return null
+            return null;
+        }
+    }
+
+    this.getValue = function ()
+    {
+        var state = getState("value");
+
+        if (typeof state != "undefined")
+        {   // If we have a value, return it
+            return state;
+        }
+        else
+        {   // Otherwise return null
+            return null;
+        }
+    }
+
+    this.setUser = function (user)
+    {   // If we have a user, set status.user = user
+        if (typeof user != "undefined") {setState(user.toString(), "user");}
+    }
+
+    this.setValue = function (value)
+    {   // If we have a value, set status.value = value
+        if (typeof value != "undefined") {setState(value.toString(), "value");}
     }
 
     // We want to initialize the Square when it is created.
